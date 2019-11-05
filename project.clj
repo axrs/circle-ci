d2afc14fb228140e4ae0daabd22252c23258bd33
Alexander Scott <xander@axrs.io>
2019-11-05 20:12:05 +1000

    Clear screen on watch start and recur

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.axrs.cli/circle-ci "0.1.0"
+(defproject io.axrs.cli/circle-ci "0.1.1"
   :description "A CircleCI CLI"
   :license "Eclipse Public License - v 2.0"
   :url "https://github.com/axrs/circle-ci"
